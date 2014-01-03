22dc0e5aca0fee6013c554812a7987b2fd4c2a11
Ezequiel <zeki-san@hotmail.com>
2014-01-03 05:25:53 -0800

    hoje

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,11 +1,2 @@
 module PagesHelper
-	# Return a title
-	def title
-		base_title = "Ruby on Rails Tutorial Sample App"
-		if @title.nil?
-			base_title
-		else
-			"#{base_title} | #{@title}"
-		end
-	end
 end
